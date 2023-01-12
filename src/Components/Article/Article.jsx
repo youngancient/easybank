dee02c5e3f9dd5198e9c01ed3dd484cb082c7a00
Jude Tochy <judetochyokoye@gmail.com>
2023-01-12 18:23:48 +0100

    finished sections and responsiveness, footer and animation left

--- src/Components/Article/Article.jsx
+++ src/Components/Article/Article.jsx
@@ -6,9 +6,13 @@ const Article = ({alt,imgSrc,heading,text,author}) => {
             <div className="article-img">
                 <img src={imgSrc} alt={alt} />
             </div>
-            <span><h5>{author}</h5></span>
-            <a href="#" className=""><h4>{heading}</h4></a>
-            <p>{text}</p>
+            <div className="pad">
+                <span><h5>{author}</h5></span>
+                <div className="link">
+                    <a href="#" className=""><h4>{heading}</h4></a>
+                    <p>{text}</p>
+                </div>
+            </div>
         </div>
      );
 }
--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,7 +1,38 @@
+import Button from "../Button/Button";
+
 const Footer = () => {
     return ( 
         <footer>
-            I am the footer
+            <section>
+                <div className="footer">
+                    <div className="one">
+                        <div className="f-logo">
+                            <img src="/assets/logo.svg" alt="footer logo" className="" />
+                        </div>
+                        <div className="socials">
+                            <a href="#" className=""><i className="fa-brands fa-facebook-f"></i></a>
+                            <a href="#" className=""><i className="fa-brands fa-youtube"></i></a>
+                            <a href="#" className=""><i className="fa-brands fa-twitter"></i></a>
+                            <a href="#" className=""><i className="fa-brands fa-pinterest"></i></a>
+                            <a href="#" className=""><i className="fa-brands fa-instagram"></i></a>
+                        </div>
+                    </div>
+                    <div className="two">
+                        <a href="#" className="">About Us</a>
+                        <a href="#" className="">Contact</a>
+                        <a href="#" className="">Blog</a>
+                    </div>
+                    <div className="three">
+                        <a href="#" className="">Career</a>
+                        <a href="#" className="">Support</a>
+                        <a href="#" className="">Privacy Policy</a>
+                    </div>
+                    <div className="four">
+                        <Button />
+                        <p className="copyright">© Easybank. All Rights Reserved</p>
+                    </div>
+                </div>
+            </section>
         </footer>
      );
 }
