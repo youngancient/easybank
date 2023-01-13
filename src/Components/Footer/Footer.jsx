d8e7e6b626ff852dc03edd2561a9dc52e36cbfd3
Jude Tochy <judetochyokoye@gmail.com>
2023-01-13 02:42:07 +0100

    i think i am done with the responsive designs, we take animations down tonight babe

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,3 +1,4 @@
+import './style.css';
 import Button from "../Button/Button";
 
 const Footer = () => {
@@ -5,29 +6,41 @@ const Footer = () => {
         <footer>
             <section>
                 <div className="footer">
-                    <div className="one">
+                    <div className="one fm">
                         <div className="f-logo">
-                            <img src="/assets/logo.svg" alt="footer logo" className="" />
+                            <img src="/assets/footerlogo.svg" alt="footer logo" className="" />
                         </div>
                         <div className="socials">
-                            <a href="#" className=""><i className="fa-brands fa-facebook-f"></i></a>
+                            <a href="#" className="first" style={{marginLeft:0}}><i className="fa-brands fa-square-facebook"></i></a>
                             <a href="#" className=""><i className="fa-brands fa-youtube"></i></a>
                             <a href="#" className=""><i className="fa-brands fa-twitter"></i></a>
                             <a href="#" className=""><i className="fa-brands fa-pinterest"></i></a>
                             <a href="#" className=""><i className="fa-brands fa-instagram"></i></a>
                         </div>
                     </div>
-                    <div className="two">
-                        <a href="#" className="">About Us</a>
-                        <a href="#" className="">Contact</a>
+                    <div className="two fm">
+                        <div className="sc" style={{marginTop: 0}}>
+                            <a href="#" className="">About Us</a>
+                        </div>
+                        <div className="sc">
+                            <a href="#" className="">Contact</a>
+                        </div>
+                        <div className="sc">
                         <a href="#" className="">Blog</a>
+                        </div> 
                     </div>
-                    <div className="three">
-                        <a href="#" className="">Career</a>
-                        <a href="#" className="">Support</a>
-                        <a href="#" className="">Privacy Policy</a>
-                    </div>
-                    <div className="four">
+                    <div className="three fm">
+                        <div className="sc" style={{marginTop: 0}}>
+                                <a href="#" className="">Career</a>
+                            </div>
+                            <div className="sc">
+                                <a href="#" className="">Support</a>
+                            </div>
+                            <div className="sc">
+                                <a href="#" className="">Privacy Policy</a>
+                            </div> 
+                        </div>
+                    <div className="four fm">
                         <Button />
                         <p className="copyright">© Easybank. All Rights Reserved</p>
                     </div>
