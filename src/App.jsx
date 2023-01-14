af0c0fd5d4745f3e2afe090ed019a7205f89416e
Jude Tochy <judetochyokoye@gmail.com>
2023-01-14 14:25:51 +0100

    oshu bare ma re o eh

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,36 @@
-import { useState, Component } from 'react';
+import { useState, useEffect, CSSProperties} from 'react';
 import './App.css';
 import Header from './Components/Header/Header';
 import Main from './Components/Main/Main';
 import Footer from './Components/Footer/Footer';
+import ClipLoader from "react-spinners/ClipLoader";
+import BounceLoader from "react-spinners/BounceLoader";
+
+
 
 function App() {
   const [isClicked, setClick] = useState(false);
+  const [isLoading, setLoading] = useState(true);
+
+  useEffect(()=>{
+    setTimeout(() => {
+      setLoading(false);
+    }, 1000);
+  },[])
   return (
     <div className={`App ${isClicked ? `shorten` : ''} `}>
-        <Header isClicked={isClicked} setClick={setClick} />
-        <Main/>
-        <Footer/>
+        {
+          isLoading ?
+          <div className="loader">
+            <BounceLoader color={'hsl(136, 65%, 51%)'} size={50} />
+          </div>
+          : 
+          <>
+            <Header isClicked={isClicked} setClick={setClick} />
+            <Main/>
+            <Footer/>
+          </>
+        }
     </div>
   )
 }
