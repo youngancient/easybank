af0b4deab571a33ef11acd055d2ce6eee27ad4f3
Jude Tochy <judetochyokoye@gmail.com>
2023-01-18 00:00:11 +0100

    refactored the codebase by creating an external file called data.jsx in a new dir Utils for externalisimg data

---
+++ src/Utils/Data.jsx
@@ -0,0 +1,67 @@
+
+
+export const rData = [
+    {
+      id: 1,
+      alt: "icon-online",
+      imgSrc: "/assets/icon-online.svg",
+      reasonHead: "Online Banking",
+      text: "Our modern web and mobile applications allow you to keep track of your finances wherever you are in the world.",
+    },
+    {
+      id: 2,
+      alt: "icon-budgeting",
+      imgSrc: "/assets/icon-budgeting.svg",
+      reasonHead: "Simple Budgeting",
+      text: "See exactly where your money goes each month. Receive notifications when you’re close to hitting your limits.",
+    },
+    {
+      id: 3,
+      alt: "icon-onboarding",
+      imgSrc: "/assets/icon-onboarding.svg",
+      reasonHead: "Fast Onboarding",
+      text: "We don’t do branches. Open your account in minutes online and start taking control of your finances right away.",
+    },
+    {
+      id: 4,
+      alt: "icon-api",
+      imgSrc: "/assets/icon-api.svg",
+      reasonHead: "Open API",
+      text: "Manage your savings, investments, pension, and much more from one account. Tracking your money has never been easier.",
+    },
+  ];
+  export const aData = [
+    {
+      id: 1,
+      alt: "currency",
+      author: "By Claire Robinson",
+      imgSrc: "/assets/image-confetti.jpg",
+      heading: "Receive money in any currency with no fees ",
+      text: "The world is getting smaller and we’re becoming more mobile. So why should you be forced to only receive money in a single …",
+    },
+    {
+      id: 2,
+      alt: "restuarant",
+      author: "By Wilson Hutton",
+      imgSrc: "/assets/image-restaurant.jpg",
+      heading: "Treat yourself without worrying about money",
+      text: " Our simple budgeting feature allows you to separate out your spending and set realistic limits each month. That means you …",
+    },
+    {
+      id: 3,
+      alt: "plane",
+      author: "By Wilson Hutton",
+      imgSrc: "/assets/image-plane.jpg",
+      heading: "Take your Easybank card wherever you go",
+      text: "We want you to enjoy your travels. This is why we don’t charge any fees on purchases while you’re abroad. We’ll even show you …",
+    },
+    {
+      id: 4,
+      alt: "confetti",
+      author: "By Claire Robinson",
+      imgSrc: "/assets/image-confetti.jpg",
+      heading: "Our invite-only Beta accounts are now live!",
+      text: "  After a lot of hard work by the whole team, we’re excited to launch our closed beta.It’s easy to request an invite through the site ...",
+    },
+  ];
+  
